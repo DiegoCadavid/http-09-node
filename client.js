334007d3d69b4edd3c3168876e500094f10f79f1
DiegoCadavid <dcmgames50@gmail.com>
2023-06-27 13:21:49 -0500

    first commit

---
+++ client.js
@@ -0,0 +1,29 @@
+const { Socket } = require("node:net");
+
+
+
+const httpRequest = (address, path) => {
+  return new Promise((resolve, reject) => {
+    const socket = new Socket();
+    socket.connect({ port: address.port}, () => console.log("Connected to server"));
+    socket.write(`Get ${path}`);
+    console.log("Sending request");
+    
+    socket.on("data", (data) => {
+      socket.destroy();
+      return resolve(data.toString());
+    });
+    
+    socket.on("error", (err) => {
+      reject(err);
+    });   
+  })
+  
+}
+
+const main = async () => {
+  const data = await httpRequest({ port: 3000 }, "/index.html");
+  console.log(data);
+}
+
+main();
---
+++ server.js
@@ -0,0 +1,39 @@
+const { Server } = require("node:net");
+const fs = require("node:fs/promises");
+const server = new Server();
+
+server.on("connection", (socket) => {
+  const remoteInfo = {
+    address: socket.remoteAddress,
+    port: socket.remotePort,
+    id: `${socket.remoteAddress}:${socket.remotePort}`,
+  };
+
+  socket.on("connect", () => {
+    console.log(`Client ${remoteInfo.id} Connected!`);
+  });
+
+  socket.on("data", async (data) => {
+    data.toString().startsWith("Get")
+      ? socket.write(await fs.readFile(`.${data.toString().split(" ")[1]}`))
+      : socket.write("Hello from server");
+  });
+
+  socket.on("close", () => {
+    console.log(`Client ${remoteInfo.id} disconnected`);
+  });
+
+  socket.on("error", (err) => {
+    console.log(`Client ${remoteInfo.id} error`, err?.code || err);
+  });
+});
+
+server.on("error", (err) => {
+  console.log(`Server error`, err?.code || err);
+});
+
+server.on("listening", () => {
+  console.log(`Server listening port ${server.address().port}`);
+});
+
+server.listen({ port: 3000, host: "localhost" });
